f930ead98a437d8a6caa602f93ed7c5696e534df
xxeol2 <jhj01177@naver.com>
2023-06-30 09:05:31 +0900

    boj10816 개선

--- 3주차/애쉬/[B] 10816_애쉬.py
+++ 3주차/애쉬/[B] 10816_애쉬.py
@@ -15,44 +15,29 @@ targets = list(map(int, input().split()))
 
 # [-10, -10, 2, 3, 3, 6, 7, 10, 10, 10]
 
-# start_i 랑 end_i 를 구하면 될듯
-def search(lst, target):
-    start_target = INF
-    end_target = -1
-
-    # find start_target
-    start = 0 
-    end = len(lst)-1
-    while start <= end:
+def find_start(lst, target):
+    start = 0
+    end = len(lst)
+    while start < end:
         mid = (start + end) // 2
-        if lst[mid] == target:
-            if start_target > mid:
-                start_target = mid
-                end = mid - 1
         if lst[mid] < target:
             start = mid + 1
         else:
-            end = mid - 1
-    
-    # find end_target
-    start = 0 
-    end = len(lst)-1
-    while start <= end:
+            end = mid
+    return start
+
+def find_end(lst, target):
+    start = 0
+    end = len(lst)
+    while start<end:
         mid = (start + end) // 2
-        if lst[mid] == target:
-            if end_target < mid:
-                end_target = mid
-                start = mid + 1
-        elif lst[mid] < target:
+        if lst[mid] <= target:
             start = mid + 1
         else:
-            end = mid - 1
-
-    return (start_target, end_target)
+            end = mid
+    return start
 
 for target in targets:
-    start, end = search(lst, target)
-    if start == INF:
-        print(0, end = " ")
-    else:
-        print(end-start+1, end = " ")
+    start = find_start(lst, target)
+    end = find_end(lst, target)
+    print(end-start, end= " ")
